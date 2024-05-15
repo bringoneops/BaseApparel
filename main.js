36194e61fe3b222e847d4ab58636f05b511f69c8
David Roderiques <111031981+bringoneops@users.noreply.github.com>
2024-05-15 02:39:30 +0000

    baseline

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const renderAlert = (state = 'error') => {
+const renderAlert = (state = "error") => {
   const messages = {
-    error: 'Please provide a valid email',
-    success: '<b>Yay! Thank you!</b> We’ve sent a confirmation link to your inbox.'
+    error: "Please provide a valid email",
+    success:
+      "<b>Yay! Thank you!</b> We’ve sent a confirmation link to your inbox.",
   };
 
   return `
@@ -11,33 +12,32 @@ const renderAlert = (state = 'error') => {
   `;
 };
 
-
 const init = () => {
-  const emailElement = document.querySelector('#email');
-  const formElement = document.querySelector('#form');
+  const emailElement = document.querySelector("#email");
+  const formElement = document.querySelector("#form");
   const alertElement = document.querySelector('[role="alert"]');
   const validationRegex = new RegExp(
-    emailElement.getAttribute('pattern') || '[^@]+@[^.]+..+',
-    'i'
+    emailElement.getAttribute("pattern") || "[^@]+@[^.]+..+",
+    "i",
   );
-  
-  emailElement.removeAttribute('required');
-  emailElement.removeAttribute('pattern');
-  formElement.setAttribute('novalidate', '');
-  
-  formElement.addEventListener('submit', evt => {
+
+  emailElement.removeAttribute("required");
+  emailElement.removeAttribute("pattern");
+  formElement.setAttribute("novalidate", "");
+
+  formElement.addEventListener("submit", (evt) => {
     evt.preventDefault();
 
     if (!validationRegex.test(emailElement.value.trim())) {
-      alertElement.innerHTML = renderAlert('error');
-      emailElement.setAttribute('aria-invalid', 'true');
+      alertElement.innerHTML = renderAlert("error");
+      emailElement.setAttribute("aria-invalid", "true");
       return;
     }
 
     // POST YOUR FORM WITH AJAX OR WHATNOT THEN RUN THIS
     formElement.parentElement.removeChild(formElement);
-    alertElement.innerHTML = renderAlert('success');
+    alertElement.innerHTML = renderAlert("success");
   });
-}
+};
 
 init();
